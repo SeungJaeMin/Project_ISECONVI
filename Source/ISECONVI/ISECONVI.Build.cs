255e861346a32cd9a07020b7fc3c3e41d156d896
SeungJMin <minlsj084@gmail.com>
2024-02-05 20:06:56 +0900

    소스코드 밀고 새로세팅

--- Source/ISECONVI/ISECONVI.Build.cs
+++ Source/ISECONVI/ISECONVI.Build.cs
@@ -7,7 +7,31 @@ public class ISECONVI : ModuleRules
 	public ISECONVI(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(
+			new string[] {
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"HeadMountedDisplay",
+			"EnhancedInput"
+			}
+		);
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"ISECONVI"
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+			}
+		);
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[] {
+			}
+		);
+
+		// Generate compile errors if using DrawDebug functions in test/shipping builds.
+		PublicDefinitions.Add("SHIPPING_DRAW_DEBUG_ERROR=1");
 	}
 }
