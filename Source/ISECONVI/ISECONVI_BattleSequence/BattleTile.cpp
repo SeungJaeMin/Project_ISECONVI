51c2367f6596cf6992467971f178b392fb3015fb
SeungJMin <minlsj084@gmail.com>
2024-02-17 23:07:57 +0900

    [implement] Battle Grid & Tile C++ 클래스 구현 / [edit] 스파인 샘플 이미지 리임포트 시도

---
+++ Source/ISECONVI/ISECONVI_BattleSequence/BattleTile.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ISECONVI_BattleSequence/BattleTile.h"
+
+// Sets default values
+ABattleTile::ABattleTile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABattleTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABattleTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
